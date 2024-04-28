8eb1e8603909187eb02477bfb2d6e7e05b6cf158
me.thangnd <me.thangnd@gmail.com>
2024-04-28 19:34:01 +0700

    ao

--- main.py
+++ main.py
@@ -1,46 +1,83 @@
 import pyautogui
 from time import sleep
 
-broswerPos = []
-soilPos = []
-seedPos = ()
-canPos = ()
-shearsPos = ()
+broswerPos = [(710, 1060), (770, 1060), (830, 1060),
+              (890, 1060), (950, 1060), (1010, 1060)]
+soilPos = [(965, 315), (1045, 315), (885, 400),
+           (965, 400), (1045, 400), (1125, 400), (805, 480), (885, 480), (965, 480), (1045, 480), (1125, 480), (1205, 480), (805, 555), (885, 555), (965, 555), (1045, 555), (1125, 555), (1205, 555), (805, 635), (885, 635), (965, 635), (1045, 635), (1125, 635), (1205, 635), (885, 720), (965, 720), (1045, 720), (1125, 720), (1205, 720), (965, 800), (1045, 800)]
+
+# broswerPos = [(710, 1060), (770, 1060), (830, 1060),
+#               (890, 1060), (950, 1060), (1010, 1060)]
+
+# (965, 315), (1045, 315),
+# (885, 400), (965, 400),(1045, 400),(1125, 400),
+# (805, 480), (885, 480), (965, 480),(1045, 480),(1125, 480),(1205,480),
+# (805, 555), (885, 555), (965, 555),(1045, 555),(1125, 555),(1205,555),
+# (805, 635), (885, 635), (965, 635),(1045, 635),(1125, 635),(1205,635),
+#  (885, 720), (965, 720),(1045, 720),(1125, 720),(1205,720),
+# (965, 800), (1045, 800)
 
 
 def plant():
-    pyautogui.click(x=seedPos[0], y=seedPos[1])
+    pyautogui.press("1")
     for pos in soilPos:
-        pyautogui.click(x=pos[0], y=pos[1], duration=0.2)
+        pyautogui.click(x=pos[0], y=pos[1], clicks=2, interval=0.07)
     pyautogui.press("esc")
 
 
 def water():
-    pyautogui.click(x=canPos[0], y=canPos[1])
+    pyautogui.press("2")
     for pos in soilPos:
-        pyautogui.click(x=pos[0], y=pos[1], duration=0.2)
+        pyautogui.click(x=pos[0], y=pos[1], clicks=2, interval=0.07)
     pyautogui.press("esc")
 
 
 def harvest():
-    pyautogui.click(x=shearsPos[0], y=shearsPos[1])
+    pyautogui.press("3")
     for pos in soilPos:
-        pyautogui.click(x=pos[0], y=pos[1], duration=0.2)
+        pyautogui.click(x=pos[0], y=pos[1], clicks=2, interval=0.07)
     pyautogui.press("esc")
 
 
-if __name__ == "__main__":
-    turn = 1
+def autoPop(turn, numofBros, shearFirst):
+    numberofBros = broswerPos[:numofBros]
+    t = turn
 
-    while turn > 0:
-        turn -= 1
-        for bros in broswerPos:
-            sleep(1)
+    while t > 0:
+
+        for bros in numberofBros:
+            pyautogui.click(x=bros[0], y=bros[1])
+            if t != turn or shearFirst:
+                harvest()
+                sleep(1)
+            else:
+                pass
             plant()
+            sleep(1)
             water()
 
-        sleep(60)
+        if t == 1:
+            for bros in numberofBros:
+                pyautogui.click(x=bros[0], y=bros[1])
+                harvest()
+                sleep(2)
 
-        for bros in broswerPos:
-            sleep(1)
-            harvest()
+        t -= 1
+
+
+autoPop(turn=5, numofBros=6, shearFirst=False)
+
+# if __name__ == "__main__":
+#     turn = 4
+#     while turn > 0:
+#         turn -= 1
+#         for bros in broswerPos:
+#             pyautogui.click(x=bros[0], y=bros[1])
+#             plant()
+#             water()
+
+#         sleep(35)
+
+#         for bros in broswerPos:
+#             pyautogui.click(x=bros[0], y=bros[1])
+#             harvest()
